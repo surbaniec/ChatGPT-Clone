f3787e779c65d58a8abd12906715fd59029ddec3
surbaniec <radsler1999@gmail.com>
2023-02-11 21:46:06 +0100

    Add nextAuth, add SessionProvider, add login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,35 @@
 import Sidebar from '@/components/Sidebar';
 import './globals.css';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import Login from '@/components/Login';
+import { SessionProvider } from '@/components/SessionProvider';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang='en'>
       <head />
       <body>
-        <div className='flex'>
-          <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
-            <Sidebar />
-          </div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className='flex'>
+              <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+                <Sidebar />
+              </div>
 
-          {/* toast */}
-          <div className='bg-[#343551] flex-1'>{children}</div>
-        </div>
+              {/* toast */}
+              <div className='bg-[#343551] flex-1'>{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,10 @@
+'use client';
+import { useSession, signOut } from 'next-auth/react';
 import NewChat from './NewChat';
 
 function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className='p-2 flex flex-col h-screen'>
       <div className='flex-1'>
@@ -9,6 +13,14 @@ function Sidebar() {
           <div></div>
         </div>
       </div>
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt='profile picture'
+          className='h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50'
+        />
+      )}
     </div>
   );
 }
